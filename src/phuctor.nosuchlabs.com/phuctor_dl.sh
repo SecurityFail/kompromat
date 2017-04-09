c8ccbfccc85f85dc8db72e6fcc92c3f4c06301d8
Benny Baumann <BenBE@geshi.org>
2017-04-09 17:50:33 +0200

    upd: Updated some source URLs

--- src/phuctor.nosuchlabs.com/phuctor_dl.sh
+++ src/phuctor.nosuchlabs.com/phuctor_dl.sh
@@ -3,6 +3,7 @@
 for a in dupes phuctored sadmods; do
     echo $a
     cd $a
-    curl "http://phuctor.nosuchlabs.com/$a" | grep -Po '/gpgkey/.*?(?=")'| cut -d/ -f3 | xargs -i echo http://phuctor.nosuchlabs.com/download/{} | xargs --max-args=128 --max-procs=8 wget -qc --show-progress --progress=bar
+    curl -m 86400 --connect-timeout 600 "http://phuctor.nosuchlabs.com/$a" | grep -Po '/gpgkey/.*?(?=")'| cut -d/ -f3 | xargs -i echo http://phuctor.nosuchlabs.com/download/{} | xargs --max-args=128 --max-procs=8 wget -qc --show-progress --progress=bar
+    curl -m 86400 --connect-timeout 600 "http://siphnos.mkj.lt/phuctor-stats/$a.html" | grep -Po '/gpgkey/.*?(?=")'| cut -d/ -f3 | xargs -i echo http://phuctor.nosuchlabs.com/download/{} | xargs --max-args=128 --max-procs=8 wget -qc --show-progress --progress=bar
     cd ..
 done
